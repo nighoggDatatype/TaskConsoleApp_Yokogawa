1fb62e9c38c696c99699400e81d3a34581ff5e22
nighoggDatatype <hosherman1965@gmail.com>
2024-10-22 13:12:43 +0800

    Add unit tests for MyTask Class

--- TaskConsoleApp.Tests/TaskTests.cs
+++ TaskConsoleApp.Tests/TaskTests.cs
@@ -1,6 +1,7 @@
-namespace TaskConsoleApp.Tests;
+using TaskConsoleApp.Task;
 
-public class Tests
+namespace TaskConsoleApp.Tests;
+public class TaskTests
 {
     [SetUp]
     public void Setup()
@@ -8,8 +9,25 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void TestInitAndGet()
     {
-        Assert.Pass();
+        DateTime now = DateTime.Now;
+        MyTask task = new("Example Task", DateOnly.FromDateTime(now));
+        Assert.That(task.TaskName, Is.EqualTo("Example Task"));
+        Assert.That(task.Date, Is.EqualTo(DateOnly.FromDateTime(now)));
+        Assert.That(task.CurrentStatus, Is.EqualTo(MyTaskStatus.Pending));
+    }
+
+    [Test]
+    public void TestSet()
+    {
+        DateTime now = DateTime.Now;
+        MyTask task = new("Example Task", DateOnly.FromDateTime(now));
+        task.TaskName = "Different Name";
+        task.CurrentStatus = MyTaskStatus.Completed;
+        Assert.That(task.TaskName, Is.EqualTo("Different Name"));
+        Assert.That(task.Date, Is.EqualTo(DateOnly.FromDateTime(now)));
+        Assert.That(task.CurrentStatus, Is.EqualTo(MyTaskStatus.Completed));
+
     }
 }
